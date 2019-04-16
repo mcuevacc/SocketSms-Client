6ae33517135045de45b1ac21c2ddbd6aebbaa848
Miguel <miguel.cueva.l@uni.pe>
2019-04-16 13:24:56 -0500

    fix

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var http = require('http');
+var request = require('request');
+var options = {
+          uri: 'http://localhost/api-sms/web/app_dev.php/sms/send',
+          method: 'POST',
+          json: null
+        };
 
 var net = require('net');
 var HOST = '0.0.0.0';
@@ -19,16 +24,15 @@ client.on('ready', ()=>{
 
 client.on('data', (data)=>{
     try {
-        console.log("Sending SMS: "+data);    
-    	obj = JSON.parse(data);    
+        console.log("Sending SMS: "+data);
 
-        http.get('http://localhost/api-sms/web/app_dev.php/sms/send?number='+obj.number+'&text='+obj.text, (resp) => {
-            resp.on('data', (data) => {
-            	console.log('Response: '+data);
-            });
-        }).on("error", (err) => {
-    	  console.log("Error: " + err.message);
-    	});
+    	options.json = JSON.parse(data);
+        request(options, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log(body)
+          }
+        });
+        
     }catch(error){
         console.error(error);
     }
